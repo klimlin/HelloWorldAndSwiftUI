ae623812cd76ee9efa4b3f06e12f72f536c2bf24
Alina <linalevel5@gmail.com>
2023-01-16 13:46:01 +0300

    Initial Commit

---
+++ HelloWorldAndSwiftUI/HelloWorldAndSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAndSwiftUIApp.swift
+//  HelloWorldAndSwiftUI
+//
+//  Created by MAcbook on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAndSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
